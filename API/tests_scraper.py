010719059eec0e75de41bd6697a224fd31d0508a
Julien <19209856+FLOKITOTO@users.noreply.github.com>
2022-02-25 18:45:51 +0100

    Update tests_scraper.py

--- API/tests_scraper.py
+++ API/tests_scraper.py
@@ -9,7 +9,6 @@ class TestScrapper(TestCase):
     def setUp(self): 
         self.scrapper = Scraped()
         self.scrapper.read_dict()
-        self.scrapper.iterate_table()
 
     def test_dict(self):
         expected = {'Program Name': {0: 'Python', 1: 'Pascal', 2: 'Lisp', 3: 'D#', 4: 'Cobol', 5: 'Fortran', 6: 'Haskell'}, 'Internet Points': {0: 932914021, 1: 532, 2: 1522, 3: 12, 4: 3, 5: 52124, 6: 24}, 'Kittens?': {0: 'Definitely', 1: 'Unlikely', 2: 'Uncertain', 3: 'Possibly', 4: 'No.', 5: 'Yes.', 6: 'lol.'}}
